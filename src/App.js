7882f73a0b147bfc73fbb90fc3bdcf85f06d2973
jgbpessoa <jgbpessoa@gmail.com>
2022-09-16 20:50:41 -0300

    css Function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import Button from "./components/Button";
+import { css } from "@stitches/react";
+
+const title = css({
+  fontFamily: "sans-serif",
+  variants: {
+    type: {
+      primary: {
+        textDecoration: "underline",
+        color: "black",
+      },
+      secondary: {
+        color: "Gray",
+      },
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <h1>Introduction to Stitches</h1>
+      <h1 className={title({ type: "primary" })}>Introduction to Stitches</h1>
+      <h2 className={title({ type: "secondary" })}>
+        Let's learn how to work with it
+      </h2>
       <Button>Simple Button</Button>
     </div>
   );
