ef12f5bc17201da5c13ac4604a092f65fe00a5ba
jgbpessoa <jgbpessoa@gmail.com>
2022-09-18 13:39:06 -0300

    globalCss Function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Button from "./components/Button";
-import { css } from "@stitches/react";
+import { css, globalCss } from "@stitches/react";
 
 const title = css({
   fontFamily: "sans-serif",
+  marginBottom: "1rem",
   variants: {
     type: {
       primary: {
@@ -16,7 +17,20 @@ const title = css({
   },
 });
 
+const globalStyles = globalCss({
+  body: {
+    background: "AntiqueWhite",
+    margin: "2rem 1rem",
+  },
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+  },
+});
+
 function App() {
+  globalStyles();
   return (
     <div className="App">
       <h1 className={title({ type: "primary" })}>Introduction to Stitches</h1>
